176705bd5117b111990b8818bfa061be013df943
Frad LEE <fradser@gmail.com>
2022-05-27 20:54:46 +0800

    style(c&r): format by prettier

--- complete-and-await-reply/OFCompleteAwaitReply.omnifocusjs/Resources/aboutThisPlugin.js
+++ complete-and-await-reply/OFCompleteAwaitReply.omnifocusjs/Resources/aboutThisPlugin.js
@@ -1,41 +1,42 @@
-(() => {
-	// USER-EDITABLE DEFAULTS
-	var authorName = "Frad LEE"
-	var descriptionString = "Mark the currently selected task as complete and add a new task to await the reply."
-	var companyURL = 'https://frad.me'
+;(() => {
+  // USER-EDITABLE DEFAULTS
+  var authorName = 'Frad LEE'
+  var descriptionString =
+    'Mark the currently selected task as complete and add a new task to await the reply.'
+  var companyURL = 'https://frad.me'
 
-	const action = new PlugIn.Action(function(selection){
-		let versNum = this.plugIn.version.versionString
-		let pluginName = this.plugIn.displayName
-		let pluginID = this.plugIn.identifier
-		let pluginLibraries = this.plugIn.libraries
-		if (pluginLibraries.length != 0){
-			let libraryNames = []
-			pluginLibraries.forEach(function(aLibrary){
-				let libraryName = aLibrary.name
-				let libraryVersion = aLibrary.version.versionString
-				let displayString = libraryName + ' v' + libraryVersion
-				libraryNames.push(displayString)
-			})
-			var libraryNamesString = "LIBRARIES:"
-			libraryNamesString += '\n' + libraryNames.join('\n')
-		} else {
-			var libraryNamesString = "This plugin has no libraries."
-		}
-		let yearString = new Date().getFullYear().toString()
-		let alertTitle = pluginName + ' v.' + versNum
-		let alertMessage = `©${yearString} ${authorName}`
-		alertMessage += '\n' + pluginID
-		alertMessage += '\n' + companyURL
-		alertMessage += '\n\n' + descriptionString
-		alertMessage += '\n\n' + libraryNamesString
-		new Alert(alertTitle, alertMessage).show()
-	});
+  const action = new PlugIn.Action(function (selection) {
+    let versNum = this.plugIn.version.versionString
+    let pluginName = this.plugIn.displayName
+    let pluginID = this.plugIn.identifier
+    let pluginLibraries = this.plugIn.libraries
+    if (pluginLibraries.length != 0) {
+      let libraryNames = []
+      pluginLibraries.forEach(function (aLibrary) {
+        let libraryName = aLibrary.name
+        let libraryVersion = aLibrary.version.versionString
+        let displayString = libraryName + ' v' + libraryVersion
+        libraryNames.push(displayString)
+      })
+      var libraryNamesString = 'LIBRARIES:'
+      libraryNamesString += '\n' + libraryNames.join('\n')
+    } else {
+      var libraryNamesString = 'This plugin has no libraries.'
+    }
+    let yearString = new Date().getFullYear().toString()
+    let alertTitle = pluginName + ' v.' + versNum
+    let alertMessage = `©${yearString} ${authorName}`
+    alertMessage += '\n' + pluginID
+    alertMessage += '\n' + companyURL
+    alertMessage += '\n\n' + descriptionString
+    alertMessage += '\n\n' + libraryNamesString
+    new Alert(alertTitle, alertMessage).show()
+  })
 
-	// routine determines if menu item is enabled
-	action.validate = function(selection){
-		return true
-	};
+  // routine determines if menu item is enabled
+  action.validate = function (selection) {
+    return true
+  }
 
-	return action;
-})();
+  return action
+})()
